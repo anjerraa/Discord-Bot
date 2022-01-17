26483538842a54165391a45eab283c6bb3d57207
Mai Pham <angellapham240@gmail.com>
2022-01-18 00:30:16 +1100

    Wrote code to generate a welcome image when a user joins the server

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ const Discord = require("discord.js");
 require("dotenv").config();
 const TOKEN = process.env.TOKEN;
 
+// Import the generateImage function from generateImage.js.
+const generateImage = require("./generateImage");
+
 // Create a bot client to access the Discord API.
 const client = new Discord.Client({
     // Add intents - things that the Discord bot is going to look out for.
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 });
 
@@ -23,6 +27,19 @@ client.on("messageCreate", (message) => {
     if (message.content == "hi") {
         message.reply("Hello World")
     }
+});
+
+// Event: member joins server -> send a welcome message ------------------------
+
+// ID of the channel where the bot will send the welcome message.
+const welcomeChannelId = "932579759492304907"
+
+client.on("guildMemberAdd", async (member) => {
+    const welcomeImage = await generateImage(member);
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `<@${member.id}> Welcome to the server!`,
+        files: [welcomeImage]
+    });
 });
 
 client.login(TOKEN);
