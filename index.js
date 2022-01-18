1f420cec85283bbfd3bc53a1843e0e95512c1114
Mai Pham <angellapham240@gmail.com>
2022-01-18 14:49:38 +1100

    Cleaned up the index file. temporarily removed the welcome image feature, will add it back later

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const Discord = require("discord.js");
 require("dotenv").config();
 const TOKEN = process.env.TOKEN;
 
-// Import the generateImage function from generateImage.js.
-const generateImage = require("./generateImage");
+// // Import the generateImage function from generateImage.js.
+// const generateImage = require("./generateImage");
 
 // Create a bot client to access the Discord API.
 const client = new Discord.Client({
@@ -18,28 +18,57 @@ const client = new Discord.Client({
     ]
 });
 
-// Ready event is triggered when the bot logs in successfully.
-client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}`)
-});
+// Create a bot object to contain extra informations.
+let bot = {
+    client,
+    prefix: "kp!",
+    // Discord ID of the owners of the bot.
+    owners: ["205996374204874752"]
+}
 
-client.on("messageCreate", (message) => {
-    if (message.content == "hi") {
-        message.reply("Hello World")
-    }
-});
+// Storing all the commands and events.
+// Similar to JS maps.
+// Key values of each of these maps be the name of the command or event.
+client.commands = new Discord.Collection();
+client.events = new Discord.Collection();
 
-// Event: member joins server -> send a welcome message ------------------------
+// LOADING EVENTS AND COMMANDS -----------------------------------------------------------
 
-// ID of the channel where the bot will send the welcome message.
-const welcomeChannelId = "932579759492304907"
+// Creating reload events and commands functions.
+client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload);
+client.loadCommands = (bot, reload) => require("./handlers/commands")(bot, reload);
 
-client.on("guildMemberAdd", async (member) => {
-    const welcomeImage = await generateImage(member);
-    member.guild.channels.cache.get(welcomeChannelId).send({
-        content: `<@${member.id}> Welcome to the server!`,
-        files: [welcomeImage]
-    });
-});
+// Call the loadEvents and loadCommands function with reload = false as we are loading the
+// bot for the first time.
+client.loadEvents(bot, false);
+client.loadCommands(bot, false);
+
+// MODULE EXPORTS ------------------------------------------------------------------------
+
+module.exports = bot
+
+// // Ready event is triggered when the bot logs in successfully.
+// client.on("ready", () => {
+//     console.log(`Logged in as ${client.user.tag}`)
+// });
+
+// client.on("messageCreate", (message) => {
+//     if (message.content == "hi") {
+//         message.reply("Hello World")
+//     }
+// });
+
+// // Event: member joins server -> send a welcome message ------------------------
+
+// // ID of the channel where the bot will send the welcome message.
+// const welcomeChannelId = "932579759492304907"
+
+// client.on("guildMemberAdd", async (member) => {
+//     const welcomeImage = await generateImage(member);
+//     member.guild.channels.cache.get(welcomeChannelId).send({
+//         content: `<@${member.id}> Welcome to the server!`,
+//         files: [welcomeImage]
+//     });
+// });
 
 client.login(TOKEN);
